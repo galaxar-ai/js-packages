1628910696a54d2a31a3b0631fabf2a7de4ea1b0
Eric Lee <ericleeeee7@gmail.com>
2023-05-14 15:06:00 +1000

    fix: minimize dist files

--- libs/jsonx/cjs/index.js
+++ libs/jsonx/cjs/index.js
@@ -1,18 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-Object.defineProperty(exports, "default", {
-    enumerable: true,
-    get: function() {
-        return _Jxs.default;
-    }
-});
-const _Jxs = /*#__PURE__*/ _interop_require_default(require("./Jxs"));
-function _interop_require_default(obj) {
-    return obj && obj.__esModule ? obj : {
-        default: obj
-    };
-}
-
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"default",{enumerable:true,get:function(){return _Jxs.default}});const _Jxs=_interop_require_default(require("./Jxs"));function _interop_require_default(obj){return obj&&obj.__esModule?obj:{default:obj}}
 //# sourceMappingURL=index.js.map
